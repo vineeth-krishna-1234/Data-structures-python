e38e3a73b93ee1fe8a5dcb984094f4a4d10af3e0
vineeth-krishna-1234 <vineethkrishna060@gmail.com>
2022-06-09 20:34:15 +0530

    update 1.0

--- data structure/doubly linked list.py
+++ data structure/doubly linked list.py
@@ -1,8 +1,3 @@
-from platform import node
-from time import sleep
-from pkg_resources import empty_provider
-
-
 class Node:
     def __init__(self,data):
         self.prev=None
@@ -14,18 +9,17 @@ class Doubly_linked_list:
     def Include_Data(self):
         prevpointer=None
         empty_pointer=None
-        next_pointer=None
         for i in range(5):
             Newnode=Node(input("Enter Node value"))
             if not self.head:
                 self.head=Newnode
                 empty_pointer=self.head
-                prevpointer=self.head
             else:
                 empty_pointer.next=Newnode
                 prevpointer=empty_pointer
                 empty_pointer=empty_pointer.next
                 empty_pointer.prev=prevpointer
+    
     def Display(self):
         Display_pointer=self.head
         while Display_pointer:
